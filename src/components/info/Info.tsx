ed8b3c7f4faaeea402e61cae82e2b6a906f067e8
Arpit Chugh <arpitchugh.jbd.ac@gmail.com>
2023-05-14 18:12:13 +0530

    sidebar menu and company profile completed

--- src/components/info/Info.tsx
+++ src/components/info/Info.tsx
@@ -1,7 +1,10 @@
 'use client';
 
-import { Tabs, TabsProps, Typography } from 'antd';
 import styles from './Info.module.scss';
+import { Typography, Image as Img, Tabs } from 'antd';
+import Image from 'next/image';
+import { useState } from 'react';
+import { HiDotsVertical } from 'react-icons/hi';
 
 const topMenu = [
 	{
@@ -25,9 +28,6 @@ const topMenu = [
 		label: `Internal Actions`,
 	},
 ];
-
-
-
 
 const SideBar_items = [
 	{
@@ -57,6 +57,14 @@ const SideBar_items = [
 ];
 
 const Info: React.FC = () => {
+	const [activeTopMenuItem, setActiveTopMenuItem] = useState('1');
+	const handleTopMenuClick = (key: string): any => {
+		setActiveTopMenuItem(key);
+	};
+	const [activeSideMenuItem, setActiveSideMenuItem] = useState('1');
+	const handleSideMenuClick = (key: string): any => {
+		setActiveSideMenuItem(key);
+	};
 	return (
 		<section className={styles['project-scope-recap']}>
 			<div className={styles.header}>
@@ -77,24 +85,103 @@ const Info: React.FC = () => {
 					</div>
 				</div>
 			</div>
-            {/*  topMenu */}
+			{/*  topMenu */}
 			<div className={styles.topMenu}>
 				{topMenu.map(item => (
-					<div key={item.key} className={styles.menuItem}>
-						{item.label}
+					<div
+						key={item.key}
+						className={styles.menuItem}
+						onClick={() => {
+							handleTopMenuClick(item.key);
+						}}
+					>
+						<div
+							className={`${
+								activeTopMenuItem === item.key ? styles.selectedTopMenuItem : ''
+							}`}
+						>
+							{item.label}
+						</div>
 					</div>
 				))}
 			</div>
+
 			{/* SideBar */}
 			<div className={styles.content}>
 				<div className={styles.sidebarMenu}>
 					{SideBar_items.map(item => (
-						<div key={item.key} className={styles.menuItem}>
-							{item.label}
+						<div
+							key={item.key}
+							className={styles.menuItem}
+							onClick={() => {
+								handleSideMenuClick(item.key);
+							}}
+						>
+							<div
+								className={`${
+									activeSideMenuItem === item.key
+										? styles.selectedSideMenuItem
+										: ''
+								}`}
+							>
+								{item.label}
+							</div>
 						</div>
 					))}
 				</div>
 				{/* <CompanyProfile /> */}
+				<div className={styles['company-profile']}>
+					<div className={styles.header}>
+						<Typography.Title className={styles.title}>
+							Company Profile
+						</Typography.Title>
+						<HiDotsVertical size={20} className={styles['three-dots']} />
+						<div className={styles.options}>
+							<Image
+								src='/icons/download.png'
+								alt='Download'
+								width={16}
+								height={20}
+							/>
+							|
+							<Image src='/icons/hide.png' alt='Hide' width={20} height={20} />|
+							<Image
+								src='/icons/delete.png'
+								alt='Delete'
+								width={20}
+								height={20}
+							/>
+						</div>
+					</div>
+					<div className={styles.background}>
+						<Img
+							src='/images/company-profile.png'
+							alt='Company Profile'
+							preview={false}
+							className={styles.image}
+						/>
+						<div className={styles.wework}>
+							<Img preview={false} src='/images/wework.png' alt='Wework' />
+							<Typography.Text className={styles.subtitle}>
+								For all the ways you work, we are here
+							</Typography.Text>
+						</div>
+						<div className={styles.options}>
+							<Img
+								src='/images/other-options.png'
+								alt='Other Options'
+								preview={false}
+							/>
+						</div>
+					</div>
+					<div className={styles.footer}>
+						<Img
+							src='/images/comment-share.png'
+							alt='Comment Share'
+							preview={false}
+						/>
+					</div>
+				</div>
 			</div>
 		</section>
 	);
